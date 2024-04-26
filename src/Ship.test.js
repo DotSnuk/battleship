e9f47af36c66b4cb716a3814effa7c8844cdefa3
Hampus Farner <hampusfarner@outlook.com>
2024-04-26 06:03:06 +0200

    add Gameboard files

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -11,10 +11,13 @@ describe('Ship', () => {
     expect(boat.isSunk()).not.toBeTruthy();
   });
 
+  it('Can I hit it more than its length?', () => {
+    for (let i = 0; i < 4; i += 1) boat.hit();
+    expect(boat.hit()).toBe(3);
+  });
+
   it('Can I sink it?', () => {
-    boat.hit();
-    boat.hit();
-    boat.hit();
+    for (let i = 0; i < 3; i += 1) boat.hit();
     expect(boat.isSunk()).toBeTruthy();
   });
 });
