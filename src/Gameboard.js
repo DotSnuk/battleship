28662986cc8e45e4fcac8fc7efc163fe11438a2c
Hampus Farner <hampusfarner@outlook.com>
2024-05-05 11:50:47 +0200

    getNodes function return desired nodes based on direction

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -32,36 +32,45 @@ export default class Gameboard {
     return starterShips;
   }
 
-  // have directioncheck take callbacks?
-
   static #directionCheck(dir, callback, length, startArray) {
     if (dir === 'vertical') return callback(length, startArray[1]);
     return callback(length, startArray[0]);
   }
 
-  calcWithinBounds(length, start) {
+  #calcWithinBounds(length, start) {
     const boardLength = this.board.length;
     if (start >= 0 && start + length <= boardLength) return true;
     return false;
   }
 
-  withinBounds(dir, length, startArray) {
-    const calc = this.calcWithinBounds.bind(this);
-    return Gameboard.#directionCheck(dir, calc, length, startArray);
+  #withinBounds(dir, length, startArray) {
+    const calcBounds = this.#calcWithinBounds.bind(this);
+    return Gameboard.#directionCheck(dir, calcBounds, length, startArray);
+  }
+
+  getNodes(dir, length, startArray) {
+    if (dir === 'vertical')
+      return this.board[startArray[0]].slice(
+        startArray[1],
+        startArray[1] + length,
+      );
+    const verticalArr = this.board.slice(startArray[0], startArray[0] + length);
+    const indxFromVertical = verticalArr.map(
+      subArray => subArray[startArray[1]],
+    );
+    return indxFromVertical;
+  }
+
+  spaceAvailable(dir, length, startArray) {
+    const nodes = this.getNodes(dir, length, startArray);
+    return nodes.every(node => node.ship === null);
   }
 
   placeShip(indxStartShip, dir, startX, startY) {
     // I can first do the checks to see if it fits before creating the ship
-    if (
-      this.withinBounds(dir, this.listStartships[indxStartShip][1], [
-        startX,
-        startY,
-      ])
-    )
-      return true;
+    const shipParam = this.listStartships[indxStartShip];
+    if (this.#withinBounds(dir, shipParam[1], [startX, startY])) return true;
     return false;
     // const ship = new Ship(name, length);
-
-    // need check for withinBounds and spaceAvailible
   }
 }
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -12,6 +12,20 @@ describe('Gameboard', () => {
     expect(board.board.length).toBe(10);
   });
 
+  it('test getNodes function, with vertical', () => {
+    const result = board.getNodes('vertical', 3, [2, 3]);
+    expect(result.length).toBe(3);
+  });
+
+  it('test getNodes with horizontal', () => {
+    const result = board.getNodes('horizontal', 3, [2, 3]);
+    expect(result.length).toBe(3);
+  });
+
+  it('test spaceAvailable', () => {
+    expect(board.spaceAvailable('horizontal', 3, [2, 3])).toBeTruthy();
+  });
+
   it('Place ship', () => {
     expect(board.placeShip(1, 'vertical', 2, 2)).toBeTruthy();
   });
